f020ab1a489d2eceb30fec52c0b89ff9a8a43404
Atneev Aleksandr <37979827+Admetty@users.noreply.github.com>
2020-10-14 17:41:50 +0800

    modified main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def new_test_func():
+    print('Something been added here')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    new_test_func()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
